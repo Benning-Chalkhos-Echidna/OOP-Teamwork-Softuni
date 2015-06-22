cccc0134efcc1500e7f43100eedd920da8acd272
Svetlozar Kirkov <svetlozark@gmail.com>
2015-06-22 18:47:54 +0300

    shorter itemcreate, added randomitem from specific type

--- Project/Project/Engine/Items/ItemCreate.cs
+++ Project/Project/Engine/Items/ItemCreate.cs
@@ -9,35 +9,58 @@ namespace Project.Engine.Items
 {
     public class ItemCreate
     {
-        private static readonly string[] HeadGearTxt =
-                File.ReadAllLines(@"..\..\Resources\headgear.txt");
-        
-        private static readonly string[] ChestGearTxt = 
-            File.ReadAllLines(@"..\..\Resources\chestgear.txt");
-
-        private static readonly string[] HandsGearTxt = 
-            File.ReadAllLines(@"..\..\Resources\handsgear.txt");
-
-        private static readonly string[] LegsGearTxt = 
-            File.ReadAllLines(@"..\..\Resources\legsgear.txt");
-
-        private static readonly string[] WeaponsTxt = 
-            File.ReadAllLines(@"..\..\Resources\weapons.txt");
-
-        private static Dictionary<Type,string[]> ItemsMap = new Dictionary<Type, string[]>
+        private static readonly Dictionary<string, string[]> ItemsMap = 
+            new Dictionary<string, string[]>
         {
-            {typeof(HeadGear),HeadGearTxt},
-            {typeof(ChestGear),ChestGearTxt},
-            {typeof(HandsGear), HandsGearTxt},
-            {typeof(LegsGear),LegsGearTxt},
-            {typeof(Weapon),WeaponsTxt}
-        }; 
+            {"HeadGear", File.ReadAllLines(@"..\..\Resources\headgear.txt")},
+            {"ChestGear", File.ReadAllLines(@"..\..\Resources\chestgear.txt")},
+            {"HandsGear", File.ReadAllLines(@"..\..\Resources\handsgear.txt")},
+            {"LegsGear", File.ReadAllLines(@"..\..\Resources\legsgear.txt")},
+            {"Weapon", File.ReadAllLines(@"..\..\Resources\weapons.txt")}
+        };
+
+        public static Item GetRandomItem(string type)
+        {
+            Random rand = new Random();
+            
+            var itemList = ItemsMap[type];
+            var randomLine = itemList[rand.Next(itemList.Length)];
+            string[] itemSplit = Regex.Split(randomLine, "\\s+");
+            
+            long id = long.Parse(itemSplit[0]);
+            string name = itemSplit[1].Replace('_', ' ');
+            int str = int.Parse(itemSplit[2]);
+            int agi = int.Parse(itemSplit[3]);
+            int intel = int.Parse(itemSplit[4]);
+            int hp = int.Parse(itemSplit[5]);
+            
+            if (type == "HeadGear")
+            {
+                return new HeadGear(id, name, str, agi, intel, hp);
+            }
+            if (type == "ChestGear")
+            {
+                return new ChestGear(id, name, str, agi, intel, hp);
+            }
+            if (type == "HandsGear")
+            {
+                return new HandsGear(id, name, str, agi, intel, hp);
+            }
+            if (type == "LegsGear")
+            {
+                return new LegsGear(id, name, str, agi, intel, hp);
+            }
+            if (type == "Weapon")
+            {
+                return new Weapon(id, name, str, agi, intel, hp);
+            }
+            
+            throw new InvalidItemException("Item creation error!");
+        }
         
         public static Item GetRandomItem()
         {
             Random rand = new Random();
-            ItemsMap = ItemsMap.OrderBy(x => rand.Next())
-                .ToDictionary(item => item.Key, item => item.Value);
 
             var randomCategory = ItemsMap.ElementAt(rand.Next(0, ItemsMap.Count)).Value;
             var randomLine = randomCategory[rand.Next(randomCategory.Length)];
