3a9a31f2297ce136d483f720c6ea590775ccc6ff
Svetlozar Kirkov <svetlozark@gmail.com>
2015-06-23 11:23:00 +0300

    itemcreate update

--- Project/Project/Engine/Items/ItemCreate.cs
+++ Project/Project/Engine/Items/ItemCreate.cs
@@ -7,7 +7,7 @@ using Project.Engine.Exceptions;
 
 namespace Project.Engine.Items
 {
-    public class ItemCreate
+    public static class ItemCreate
     {
         private const string ItemListsPath = @"..\..\Resources\item_lists\";
         private static readonly Dictionary<string, string[]> ItemsMap = 
@@ -31,16 +31,28 @@ namespace Project.Engine.Items
             
             var itemList = ItemsMap[type];
             var randomLine = itemList[rand.Next(itemList.Length)];
-            string[] itemSplit = Regex.Split(randomLine, "\\s+");
-            
-            long id = long.Parse(itemSplit[0]);
-            string subtype = itemSplit[1];
-            string name = itemSplit[2].Replace('_', ' ');
-            int str = int.Parse(itemSplit[3]);
-            int agi = int.Parse(itemSplit[4]);
-            int intel = int.Parse(itemSplit[5]);
-            int hp = int.Parse(itemSplit[6]);
-            
+            var returnStats = ReturnStats(randomLine);
+            return ReturnItem(type, (long)returnStats[0], (string)returnStats[1], 
+                (string)returnStats[2], (int)returnStats[3], (int)returnStats[4], 
+                (int)returnStats[5], (int)returnStats[6]);
+        }
+        
+        public static Item GetRandomItem()
+        {
+            Random rand = new Random();
+
+            var randomCategory = ItemsMap.ElementAt(rand.Next(0, ItemsMap.Count));
+            var randomCategoryList = randomCategory.Value;
+            var randomLine = randomCategoryList[rand.Next(randomCategoryList.Length)];
+            var returnStats = ReturnStats(randomLine);
+            return ReturnItem(randomCategory.Key, (long)returnStats[0], (string)returnStats[1],
+                (string)returnStats[2], (int)returnStats[3], (int)returnStats[4],
+                (int)returnStats[5], (int)returnStats[6]);
+        }
+
+        private static Item ReturnItem
+            (string type, long id, string subtype, string name, int str, int agi, int intel, int hp)
+        {
             if (type == "HeadGear")
             {
                 return new HeadGear(id, subtype, name, str, agi, intel, hp);
@@ -61,18 +73,13 @@ namespace Project.Engine.Items
             {
                 return new Weapon(id, subtype, name, str, agi, intel, hp);
             }
-            
+
             throw new InvalidItemException("Item creation error!");
         }
-        
-        public static Item GetRandomItem()
-        {
-            Random rand = new Random();
 
-            var randomCategory = ItemsMap.ElementAt(rand.Next(0, ItemsMap.Count)).Value;
-            var randomLine = randomCategory[rand.Next(randomCategory.Length)];
-            string[] itemSplit = Regex.Split(randomLine, "\\s+");
-            
+        private static List<object> ReturnStats(string line)
+        {
+            string[] itemSplit = Regex.Split(line, "\\s+");
             long id = long.Parse(itemSplit[0]);
             string subtype = itemSplit[1];
             string name = itemSplit[2].Replace('_', ' ');
@@ -80,29 +87,8 @@ namespace Project.Engine.Items
             int agi = int.Parse(itemSplit[4]);
             int intel = int.Parse(itemSplit[5]);
             int hp = int.Parse(itemSplit[6]);
-            
-            if (itemSplit[0].Length < 4)
-            {
-                return new HeadGear(id,subtype,name,str,agi,intel,hp);
-            }
-            if (itemSplit[0].Length == 4)
-            {
-                return new ChestGear(id, subtype, name, str, agi, intel, hp);
-            }
-            if (itemSplit[0].Length == 5)
-            {
-                return new HandsGear(id, subtype, name, str, agi, intel, hp);
-            }
-            if (itemSplit[0].Length == 6)
-            {
-                return new LegsGear(id, subtype, name, str, agi, intel, hp);
-            }
-            if (itemSplit[0].Length == 7)
-            {
-                return new Weapon(id, subtype, name, str, agi, intel, hp);
-            }
-
-            throw new InvalidItemException("Item creation error!");
-        }
+            var returnList = new List<object>{id,subtype,name,str,agi,intel,hp};
+            return returnList;
+        } 
     }
 }
