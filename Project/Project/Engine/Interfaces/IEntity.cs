4d39325a0e285d3a06b8d54df0b992e40e89c5d1
Peter Georgiev <shnogeorgiev@gmail.bg>
2015-06-29 16:26:08 +0300

    Deleted JSON and the Useless Packages

--- Project/Project/Engine/Interfaces/IEntity.cs
+++ Project/Project/Engine/Interfaces/IEntity.cs
@@ -5,13 +5,21 @@ namespace Project.Engine.Interfaces
     public interface IEntity
     {
         int Agility { get; set; }
+        
         int Attack { get; set; }
+        
         int Defense { get; set; }
+        
         EntityGender EntityGender { get; set; }
+        
         Spell EntitySpell { get; set; }
+        
         int Health { get; set; }
+        
         int Intellect { get; set; }
+        
         string Name { get; set; }
+
         int Strength { get; set; }
 
         bool isAlive { get; set; }
